58a4cd8b4c04e4c0897ba541864d943d6363655b
NadavM316111 <nadavminkowitz@gmail.com>
2026-08-17 18:57:22 -0400

    teshirt

--- app/page.tsx
+++ app/page.tsx
@@ -272,7 +272,7 @@ const TIERS = [
       "Meet and greet with the band",
       "Signed commemorative concert poster",
       "Photo with the band",
-      "Recognition on this site and on event t-shirt",
+      "Recognition on this site and receive free event t-shirt",
     ],
   },
 ];
